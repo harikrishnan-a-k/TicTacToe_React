3ebfcc87e8d03559cd328b49a7ac28507c8ef389
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2020-05-13 21:46:15 +0530

    Updates

---
+++ precache-manifest.5d8bbd916a62f2768fef5e72d9516ead.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47443656ac3212a50b0f6903ce8de2fb",
+    "url": "/TicTacToe_React/index.html"
+  },
+  {
+    "revision": "bbc717fe6f0862d91839",
+    "url": "/TicTacToe_React/static/css/2.712fe445.chunk.css"
+  },
+  {
+    "revision": "198613f723d8455b634e",
+    "url": "/TicTacToe_React/static/css/main.5dae246f.chunk.css"
+  },
+  {
+    "revision": "bbc717fe6f0862d91839",
+    "url": "/TicTacToe_React/static/js/2.a08342ce.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/TicTacToe_React/static/js/2.a08342ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198613f723d8455b634e",
+    "url": "/TicTacToe_React/static/js/main.2ad2c4dd.chunk.js"
+  },
+  {
+    "revision": "4427beac660e8eb2d62c",
+    "url": "/TicTacToe_React/static/js/runtime-main.a674447d.js"
+  },
+  {
+    "revision": "f12785bebe28f7ea8fa9aa0ced9c4d85",
+    "url": "/TicTacToe_React/static/media/click2.f12785be.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe_React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "535a4cf54d6414c06c6101b427306867",
+    "url": "/TicTacToe_React/static/media/lost.535a4cf5.mp3"
+  },
+  {
+    "revision": "62afed884bbdec0936d011919b6b7093",
+    "url": "/TicTacToe_React/static/media/winOrTie.62afed88.mp3"
+  }
+]);
